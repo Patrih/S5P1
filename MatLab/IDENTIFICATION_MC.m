c723f68da6fc441139ad9d7e01fa51d3eeb19c0a
Louis-Daniel Gaulin <gaul2525@usherbrooke.ca>
2019-10-07 16:26:42 -0400

    Try to fix the identification, but failed

--- MatLab/IDENTIFICATION_MC.m
+++ MatLab/IDENTIFICATION_MC.m
@@ -1,16 +1,51 @@
-
 clc
 clear all
+close all
 
 load('ACT_Fe_attraction.mat');
 load('ACT_Fs');
 
-figure(1)
-plot (z_m1A , Fe_m1A)
-figure(2)
-plot (z_m2A , Fe_m2A)
-figure(3)
-plot (z_pos, Fs)
+% Array to choose the displayed figures ; a one in the position displays
+%Figure  1 2 3 4 5 6 7 8 9 
+plots = [0 1 1 0 0 0 0 0 0];
 
+%figures 1 : Original data
+if plots(1)
+    figure()
+    plot (z_m1A , Fe_m1A)
+    figure()
+    plot (z_m2A , Fe_m2A)
+    figure()
+    plot (z_pos, Fs)
+end
+    
+%Building the P matrix and the Y vector
+
+P = [ones(size(z_pos)) z_pos z_pos.^2 z_pos.^3];
+Y = -1./(Fs);
+A = inv(P.'*P)*P.'*Y
+
+%Evaluating the sim to verify
+Fs_sim = -1./(A(1) + A(2).*z_pos + A(3).*z_pos.^2 + A(4).*z_pos.^3);
+
+% TODO : Repair the sim ; it doesnt work
+
+% Figure 2 : Original vs sim
+if plots(2)
+    figure()
+    hold on
+    plot (z_pos, Fs)
+    plot(z_pos,Fs_sim)
+    title('Comparaison Fs_sim')
+    legend('original','Moindre carre')
+    hold off
+end
+
+% Figure 2 : Original vs sim error
+if plots(3)
+    figure()
+    plot (z_pos, Fs_sim - Fs)
+    title('Erreur Comparaison Fs_sim')
+end
 
 
