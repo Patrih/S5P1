28fa659875b337941ee744d7799d544867815f27
Louis Etienne <louis.etienne82@orange.fr>
2019-10-07 11:03:21 -0400

    Define symbolic variables

--- MatLab/MAIN.m
+++ MatLab/MAIN.m
@@ -8,7 +8,13 @@ syms masseS     % Masse  de la sphère
 
 syms masseP inertiePx intertiePy % masse et inertie plaque
 
-syms XK YK ZK   % position inertielle des éléments fixes K (K = A, B, C, D, E, F) 
+syms XA YA ZA   % position inertielle des éléments fixes K (K = A, B, C, D, E, F) 
+syms XB YB ZB 
+syms XC YC ZK 
+syms XK YK ZK 
+syms XK YK ZK 
+syms XK YK ZK 
+
 
 syms Px Py Pz   % position de la sphère dans le repère inertiel 
 syms Vx Vy Vz   % vitesse de la sphère dans le repère inertiel
@@ -20,6 +26,8 @@ syms FA FB FC   % forces appliquée par les actionneurs sur la plaque (positif ve
 syms MA MB MC   % couple appliquée par les actionneurs sur la plaque 
 syms VA VB VC   % tension électrique appliquée aux actionneurs
 syms IA IB IC   % courant électrique dans les actionneurs 
+
+syms Ya Yb Yc   % 
 
 % ? _??? : réfère à des variables mesurées
 % ? _??? : réfère aux variables désirées ou commandées
---
+++ MatLab/MISE_EQUATIONS.m
@@ -0,0 +1,16 @@
+%-----------------------------------------------------
+% Titre : mise_equation
+% Date de création : 07-10-2019
+% Auteur : Louis Etienne
+%-----------------------------------------------------
+
+%% Definitions
+load(MAIN.m);
+
+syms ae0 ae1 ae2 ae3
+syms Za Zb Zc
+
+%% 
+Tabc = [Ya
+
+[Za, Zb, Zc]' = 
