b2e57e2f562c56fc40852021f7c8f853ed061813
Philippe Arsenault <arsp2701@usherbrooke.ca>
2019-11-19 13:50:17 -0500

    Continuity Error

--- MatLab/Functions/ErrRP.m
+++ MatLab/Functions/ErrRP.m
@@ -8,8 +8,5 @@ erp = 1/(FTBO.Numerator{:}(end)/FTBO.Denominator{:}(end-GetClass(FTBO)));
 if (GetClass(FTBO) == 0)
    erp = 1/((FTBO.Numerator{:}(end)/FTBO.Denominator{:}(end-GetClass(FTBO)))+1);
 end
-
-
-
 end
 
