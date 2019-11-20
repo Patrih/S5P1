abbd8ce47ecaa4db308dafd65a92943c0174d0dc
Philippe Arsenault <arsp2701@usherbrooke.ca>
2019-11-20 18:07:03 -0500

    Asservissement x et y

--- MatLab/Asservissement_x_y.m
+++ MatLab/Asservissement_x_y.m
@@ -11,7 +11,87 @@ addpath('Functions');
 % Les itérations pour atteindre les perf désirées
 
 load('equilibre');
+%% POUR X
 
+
+ts_s = 2;
+zeta = 0.9;
+Wn = 4/(zeta*ts_s);
+Kv = 2*zeta*Wn/(num_x(1,3))
+Kp = Wn^2/num_x(2,2)
+
+num = [Kp*num_x(2,2)];
+den = [1 Kv*num_x(1,3) num_x(2,2)*Kp];
+
+num_i = [1 0 0];
+den_i = [1 num_x(1,3)*Kv 0];
+
+is = tf(num_i,den_i)
+
+figure()
+margin(Kp*is*TF_x)
+figure
+rlocus(Kp*is*TF_x)
+figure
+rlocus(TF_x)
+
+feed = feedback(Kp*is*TF_x,1);
+
+t = linspace (0,10,10000);
+
+x = lsim(feed,ones(size(t)),t);
+a = 1.02*ones(size(t));
+b = .98*ones(size(t));
+
+figure()
+plot(t,x)
+hold on
+plot(t,a)
+plot(t,b)
+
+
+%% POUR Y
+
+
+ts_s = 2;
+zeta = 0.9;
+Wn = 4/(zeta*ts_s);
+Kv = 2*zeta*Wn/(num_y(1,3))
+Kp = Wn^2/num_y(2,2)
+
+num = [Kp*num_y(2,2)];
+den = [1 Kv*num_y(1,3) num_y(2,2)*Kp];
+
+num_i = [1 0 0];
+den_i = [1 num_y(1,3)*Kv 0];
+
+is = tf(num_i,den_i)
+
+figure()
+margin(Kp*is*TF_y)
+figure
+rlocus(Kp*is*TF_y)
+figure
+rlocus(TF_y)
+
+feed = feedback(Kp*is*TF_y,1);
+
+t = linspace (0,10,10000);
+
+x = lsim(feed,ones(size(t)),t);
+a = 1.02*ones(size(t));
+b = .98*ones(size(t));
+
+figure()
+plot(t,x)
+hold on
+plot(t,a)
+plot(t,b)
+
+
+
+
+%%
 
 %de 2 a 4
 FTBO = TF_y;
@@ -73,8 +153,6 @@ b = .98*ones(size(t));
 % hold on
 % plot(t,a)
 % plot(t,b)
-
-
 
 
 
