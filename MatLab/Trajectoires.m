e1b1228fd2705a8ebd504368cad91ce73fafa2a5
Louis-Daniel Gaulin <gaul2525@usherbrooke.ca>
2019-12-02 16:05:05 -0500

    new trajetcory for tests

--- MatLab/Trajectoires.m
+++ MatLab/Trajectoires.m
@@ -19,24 +19,28 @@ addpath('Functions');
 R = 0.0625;
 N_pts = 6;
 
-x = (0:R/N_pts:R-R/N_pts).';
-y = ones(size(x));
-y(1) = 0;
+% x = (0:R/N_pts:R-R/N_pts).';
+% y = ones(size(x));
+% y(1) = 0;
+% 
+% 
+% %loop generating y values and assuring they are not outside the radius
+% 
+% for i = 2 : N_pts
+%     while sqrt((x(i)^2+y(i)^2)) > R;
+%         y(i) = R*rand([1,1]) ;
+%     end
+% end
 
+x = [0 0.2 0.3 0.4 0.6 0.8].*R ;
+y = [0 0.8 0.6 0 -0.2 0].*R ;
 
-%loop generating y values and assuring they are not outside the radius
-
-for i = 2 : N_pts
-    while sqrt((x(i)^2+y(i)^2)) > R;
-        y(i) = R*rand([1,1]) ;
-    end
-end
 
 %Input for the back and forth speeds and Period
 
-v_des_al = 1;
+v_des_al = 0.01;
 v_des_ret = 0.5;
-Ts = 0.01;
+Ts = 1;
 
 z = 0.015;
 
@@ -53,13 +57,11 @@ z = 0.015;
 
 
 %aller
-[Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = ComputeTrajectories([x , y] , v_des_al , Ts , z);
+[Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = ComputeTrajectories([x' , y'] , v_des_al , Ts , z);
 %retour
-[Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = ComputeTrajectories([flip(x) , flip(y)] , v_des_ret , Ts , z);
+% [Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = ComputeTrajectories([flip(x) , flip(y)] , v_des_ret , Ts , z);
 
 save('Data/TRAJ_OUT' , 'Traj')
 save('Data/PARAMS_TRAJ' , 'Pi','Ltr','E','Vr','tt')
 
-
-plot(t,Traj)
 
