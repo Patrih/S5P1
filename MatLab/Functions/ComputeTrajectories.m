5fcf407f0572f69b5ea64b86123dfd87c15616ab
Louis-Daniel Gaulin <gaul2525@usherbrooke.ca>
2019-12-02 11:19:03 -0500

    Fix the Compute trajectories

--- MatLab/Functions/ComputeTrajectories.m
+++ MatLab/Functions/ComputeTrajectories.m
@@ -1,4 +1,4 @@
-function [Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = compute_trajectories(points_in , v_des , Ts)
+function [Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = ComputeTrajectories(points_in , v_des , Ts)
     % This function do a Lagrange interpolation.
     % param: points_in - (N,2) vector containing the desired points
     % param: v_des     - The desired speed
@@ -12,6 +12,8 @@ function [Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = compute_trajectories(points
     % return: Traj_BE  - TODO
 
     % Interpolation de la trajectoire
+    
+    R = 0.0625;
     
     addpath('Functions');
     
@@ -56,9 +58,7 @@ function [Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = compute_trajectories(points
     
     x_sep = zeros(N_arc+1,1);
     ite_count   = zeros(N_arc+1,1);
-    
-    
-    
+
     for i = 2:N_arc+1
         ite = 0;
         X_NR_strt = x_sep(i - 1);
@@ -74,33 +74,65 @@ function [Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = compute_trajectories(points
             F_NR = eval(vpaintegral(g_sym,x_sym,[X_NR_strt , X_NR])- L_stp);
             dF_NR = eval(subs(g_sym , X_NR));
 
-            ite = ite + 1;
+            ite = ite + 1;  
         end
-        ite_count(i) = ite;
-        x_sep(i) = X_NR;
         
-        Ltr (i,2) = Ltr (i-1)+L_stp;
+        if ite >= ite_max
+            % Detects an error from NR and displays an error
+            
+            % The plot is for debug
+            
+            x_eval = x(1) : 0.0001 : x(end)+0.0001;
+            y_eval = eval(subs(f_sym , x_eval));
+            
+            figure
+            hold on
+            plot(x , y , 'o')
+            plot(x_eval , y_eval)
+            axis equal
+            hold off
+            
+            error('ComputeTrajectories (line 72) : Reached max iteration at arc No %d' , [i]);
+
+        end
         
+            ite_count(i) = ite;
+            x_sep(i) = X_NR;
+
+            clc
+            fprintf('%d / %d \n', [i; N_arc+1]);
+
+            Ltr (i,2) = Ltr (i-1)+L_stp;
     end
-    Ltr(:,1) = 0:1:length(Ltr);
+        
+
+    
+    Ltr(:,1) = 1:1:length(Ltr);
     
     y_sep = eval(subs(f_sym , x_sep));
     
-    x_eval = x_sep(1) : 0.01 : x_sep(end);
+    x_eval = x_sep(1) : 0.0001 : x_sep(end)+0.0001;
     y_eval = eval(subs(f_sym , x_eval));
     
-    Traj = [x_sep , y_sep]
+    Traj = [x_sep , y_sep];
 
     %TODO WHEN WE KNOW THE FORMAT
     Traj_BE = 0;
     
-%     figure
-%     hold on 
-%     plot(x_sep , y_sep , 'o')
-%     plot(x_eval , y_eval)
-%     xlim ([0 10])
-%     ylim ([0 10])
-%     hold off
+    th = 0:pi/50:2*pi;
+    xunit = R * cos(th);
+    yunit = R * sin(th);
+    
+    figure
+    hold on 
+    plot(x_sep , y_sep , 'o')
+    plot(x_eval , y_eval)
+    plot(xunit, yunit)
+    plot(x,y,'x')
+    xlim ([1.1*-R , 1.1*R])
+    ylim ([1.1*-R , 1.1*R])
+    axis equal
+    hold off
 
 end
 
--- MatLab/Trajectoires.m
+++ MatLab/Trajectoires.m
@@ -4,16 +4,42 @@ clc;
 
 addpath('Functions');
 
-x = (0:2:10)';
-y = 10.*rand([1, 6])';
+R = 0.0625;
+N_pts = 6;
+
+x = (0:R/N_pts:R-R/N_pts).';
+y = ones(size(x));
+y(1) = 0;
+
+for i = 2 : N_pts
+    while sqrt((x(i)^2+y(i)^2)) > R;
+        y(i) = R*rand([1,1]) ;
+    end
+end
+
+
+
 t = 0:0.1:10;
 
-v_des = 2;
-Ts = 0.5;
+v_des_al = 1;
+v_des_ret = 1;
+Ts = 0.01;
 
-[Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = compute_trajectories([x , y] , v_des , Ts);
+th = 0:pi/50:2*pi;
+xunit = R * cos(th);
+yunit = R * sin(th);
 
+figure
+hold on
+plot(x , y)
+plot(xunit, yunit)
+axis equal
+hold off
 
+[Pi , Ltr , E , Vr , Traj , tt , Traj_BE] = ComputeTrajectories([x , y] , v_des_al , Ts);
+
+save('TRAJ_OUT' , 'Traj')
+save('PARAMS_TRAJ' , 'Pi','Ltr','E','Vr','tt')
 
 
 
