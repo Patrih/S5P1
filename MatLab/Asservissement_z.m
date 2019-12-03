00c09fc68090c92a785f27f1a9b3c6e1068e76dd
Guillaume Théberge-Dupuis <theg2902@usherbrooke.ca>
2019-12-03 12:09:51 -0500

    corection of pole

--- MatLab/Asservissement_z.m
+++ MatLab/Asservissement_z.m
@@ -20,7 +20,7 @@ addpath('Functions');
 % 8   
 % 9   
 %Figure  1 2 3 4 5 6 7 8 9
-plots = [0 0 1 0 0 0 0 0 0];
+plots = [0 1 1 1 1 0 0 0 0];
 
 %% Définitions & Loading
 
@@ -28,7 +28,7 @@ plots = [0 0 1 0 0 0 0 0 0];
 load('equilibre');
 
 %Definition
-PMd = 25;
+PMd = 26.5;
 wgd = 185;
 errech1d = -0.0004;
 errech2d = 0;
@@ -69,8 +69,10 @@ FTBO_finale = compensateur_final* TF_z;
 disp("----------------------Compensateur discret------------------------------")
 
 %Asservissement en Z
-z_pi.discret.num =    1.0e+06 *[1.179170317199179  -3.242723345839170   2.968867523214514  -0.904880742326366];
-z_pi.discret.den =    [1.000000000000000  -1.322007697316007   0.309747543689684   0.012260819356794];
+z_pi.discret.num =     [1194741.6019324022345 -3291752.0118398084305 3020232.5319271567278 -922783.40398222906515];
+z_pi.discret.den =    [1 -1.3241345061291269758 0.32474244393264728448 -0.00060793780352032093534];
+
+
 
 z_re.discret.num = [1179050.4692867584527 -3242393.7635745150037 2968565.7749685486779 -904788.77251812396571];
 z_re.discret.den = [1 -1.321805422334144442 0.30968240938627561443 0.012258339297300559212];
@@ -79,17 +81,18 @@ z_re.discret.den = [1 -1.321805422334144442 0.30968240938627561443 0.01225833929
 if cas == CasPI
     % testdiscret(compensateur_final)
 
-    %Creation du compensateur discret (Data provient de testdiscret.p)
-    numDiscret = [1179170.3171991789714 -3242723.3458391698077 2968867.5232145143673 -904880.74232636578381];
-    denDiscret = [1 -1.3220086973160070087 0.30974786569838153039 0.012260831617625414167];
-    compensateursDiscret = tf(numDiscret,denDiscret);
-
-    %Reajustement du pole > 1 a 0.99
-    [zeroes, poles, gain] = tf2zp(compensateursDiscret.Numerator{:}, compensateursDiscret.Denominator{:});
-    poles(1) = 1-1e-6;
-    [num_comp, den_comp] = zp2tf(zeroes, poles, gain);
-
-    compensateursDiscret = tf(num_comp, den_comp)
+% Obselete code, could be useful :)
+%     %Creation du compensateur discret (Data provient de testdiscret.p)
+%     numDiscret = [1179170.3171991789714 -3242723.3458391698077 2968867.5232145143673 -904880.74232636578381];
+%     denDiscret = [1 -1.3220086973160070087 0.30974786569838153039 0.012260831617625414167];
+%     compensateursDiscret = tf(numDiscret,denDiscret);
+% 
+%     %Reajustement du pole > 1 a 0.99
+%     [zeroes, poles, gain] = tf2zp(compensateursDiscret.Numerator{:}, compensateursDiscret.Denominator{:});
+%     poles(1) = 1-1e-6;
+%     [num_comp, den_comp] = zp2tf(zeroes, poles, gain);
+% 
+%     compensateursDiscret = tf(num_comp, den_comp)
     
     z_pi.continus.num = compensateur_final.Numerator{:};
     z_pi.continus.den = compensateur_final.Denominator{:};
@@ -129,15 +132,18 @@ bw = bandwidth(FTBO_finale)
 allmargin(FTBO_finale)
 
 if plots(3)
+    figure()
     margin(FTBO_finale)
 end
 
 %Rlocus
 if plots(4)
+    figure()
     rlocus(FTBO_finale)
 end
 
 %Discret
 if plots(5)
-    testdiscret(compensateurs)
+    figure()
+    testdiscret(compensateur_final)
 end 
